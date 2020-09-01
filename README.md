e587e275a1af5839906476467f014daf53f6478a
dividendninja <dividendninjahelp@gmail.com>
2020-08-31 17:56:25 -0700

    Sample Heroku Build

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Welcome to my online portfolio.
 
 Projects
 1. Stock Screener Dashboard
+
+
+
+Deployment Files
+1. Procfile
+2. Setup
+3. Requirements
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pandas as pd
+import numpy as np
+import streamlit as st
+
+def main():
+    """A Simple EDA App"""
+    st.title("Iris EDA App with streamlit")
+    st.subheader("Streamlit is cool")
+
+if __name__=='__main__':
+    main()
